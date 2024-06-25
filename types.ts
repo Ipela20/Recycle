7cb954be99d7bae0699227b648a3afd8f9ff8576
Ipela20 <96437605+Ipela20@users.noreply.github.com>
2024-06-25 22:44:05 +0000

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,10 @@
+// app/types.ts
+export type RootStackParamList = {
+  Login: undefined;
+  Home: undefined;
+  Profile: undefined; // Assure-toi que 'Profile' est bien défini ici
+  Map: undefined; // Assure-toi que 'Map' est bien défini ici
+  SortingInfo: { wasteType: string }; // Assure-toi que 'SortingInfo' est bien défini ici
+  Rewards: undefined; // Assure-toi que 'Rewards' est bien défini ici
+};
+
